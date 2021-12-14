b2a7e6170c6dbe786a908d4b32c2bbe21d865669
louisepujante <louisegwen.pujante@sun-asterisk.com>
2021-12-14 18:50:52 +0800

    [SELS-TASK][BE] Lessons Management

---
+++ backend/app/Http/Controllers/Lesson/LessonController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers\Lesson;
+
+use App\Models\Lesson;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class LessonController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Lesson  $lesson
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Lesson $lesson)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Lesson  $lesson
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Lesson $lesson)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Lesson  $lesson
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Lesson $lesson)
+    {
+        //
+    }
+}
