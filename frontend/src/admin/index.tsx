4b836835d694648ffaaf6f92ee92ad9bd3e60ae2
louisepujante <86587091+louisepujante@users.noreply.github.com>
2022-01-06 18:00:44 +0800

    Update index.tsx
    manually editing this file in github since change is not reflected

--- frontend/src/admin/index.tsx
+++ frontend/src/admin/index.tsx
@@ -3,10 +3,10 @@ import { Nav } from '@components/';
 
 
 export default function AdminDashboard() {
-    return (
-      <div>
-        <Nav className='bg-purple-200' />
-        <h1 className='text-center text-2xl mt-2'>Hello admin homepage</h1>
-      </div>
-    );
+  return (
+    <div>
+      <Nav className='bg-purple-200' />
+      <h1 className='text-center text-2xl mt-2'>Hello admin homepage</h1>
+    </div>
+  );
 }
