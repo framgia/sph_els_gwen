8fb6b24513e6a8ed476453da3b1ed0c7ffa40017
louisepujante <louisegwen.pujante@sun-asterisk.com>
2022-01-17 12:16:59 +0800

    changed implementation for adding and editing of lesson to accept choices array

--- backend/app/Http/Controllers/Lesson/LessonController.php
+++ backend/app/Http/Controllers/Lesson/LessonController.php
@@ -1,10 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\Lesson;
-
+use App\Models\Choice;
 use App\Models\Lesson;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class LessonController extends Controller
 {
@@ -18,5 +20,30 @@ class LessonController extends Controller
     {
         $withChoices = $lesson->load('choices');
         return $this->returnOne($withChoices);
+    }
+
+    public function update(Request $request, Lesson $lesson)
+    {
+        $this->checkIfAdmin();
+
+        $request->validate([
+          'word'=> ['required', 'string'],
+          'choices' => ['required', 'array', 'min:4'],
+          'choices.*.name' => ['required', 'string'],
+          'choices.*.is_correct'=> ['required', 'boolean']
+        ]);
+
+        //update lesson
+        $lesson->fill($request->only(['word']));
+        $lesson->save();
+
+        //update choices
+        foreach($request->choices as $choice) {
+          Choice::where('id', $choice['id'])->update([
+            'name' => $choice['name'],
+            'is_correct' => $choice['is_correct']            
+          ]);
+        }        
+        return $this->returnOne($lesson->load('choices'));
     }
 }
