0e520b6e95d54f71698a7e329ca71f7aeda30a6e
louisepujante <louisegwen.pujante@sun-asterisk.com>
2022-01-19 17:27:36 +0800

    added error handler for viewing all category logs of a user that doesn't exist

--- backend/app/Http/Controllers/User/UserController.php
+++ backend/app/Http/Controllers/User/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\User;
 
 use App\Models\User;
-use App\Http\Controllers\Controller;
 use App\Models\CategoryLog;
+use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Gate;
 use Illuminate\Auth\Access\HandlesAuthorization;
 use Illuminate\Auth\Access\AuthorizationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 
 class UserController extends Controller
@@ -46,6 +47,11 @@ class UserController extends Controller
 
     public function allCategoryLogs($id)
     {
+      $user = User::where('id', $id)->get();
+      if(count($user)===0) {
+        throw new HttpException(404, 'Resource not found');
+      }
+
       $category_logs = CategoryLog::where('user_id', $id)->get();
       return $this->returnAll($category_logs);
     }
